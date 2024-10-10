a233cddefd8c990188d97f1d34c3137afd25a446
bhavishraja1188 <bhavishraja99@gmail.com>
2024-10-10 13:36:39 +0530

    Uploading code: Good Turn.c

---
+++ Good Turn.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int X,Y;
+    scanf("%d%d",&X,&Y);
+    if(X+Y>6)
+    {
+        printf("YES");
+    }
+     else
+    {
+        printf("NO");
+    }
+    return 0;
+}

ff881399df4d201e461cee99b746dd0fd8bbd6d6
bhavishraja1188 <bhavishraja99@gmail.com>
2024-10-11 12:11:55 +0530

    Uploading code: Sum it.c

---
+++ Sum it.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int X,Y,Z;
+    scanf("%d%d%d",&X,&Y,&Z);
+    if(X+Y==Z)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}

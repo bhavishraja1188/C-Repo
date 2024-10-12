ee3ef84d30f6a53305f10c34ea25decfe2aea32e
bhavishraja1188 <bhavishraja99@gmail.com>
2024-10-12 23:07:54 +0530

    Uploading code: Scalene Triangle.c

---
+++ Scalene Triangle.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    if(a!=b && a!=c && b!=a && b!=c && c!=a && c!=b)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}

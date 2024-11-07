33f3ce6df72a954e55bbd84cef62edac508afb8c
bhavishraja1188 <bhavishraja99@gmail.com>
2024-11-07 23:32:11 +0530

    Uploading code: Tickets.c

---
+++ Tickets.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    int b=4*a;
+    if(b>1000)
+    {
+        printf("NO");
+    }
+    else
+    {
+        printf("YES");
+    }
+}

a0f643b93dad685bc9a8f563db845896177f17df
bhavishraja1188 <bhavishraja99@gmail.com>
2024-11-08 20:33:15 +0530

    Uploading code: Interior Design.c

---
+++ Interior Design.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d,e,f;
+    scanf("%d%d%d%d",&a,&b,&c,&d);
+    e=a+b;
+    f=c+d;
+    if(e>=f)
+    {
+        printf("%d",f);
+    }
+    else
+    {
+      printf("%d",e);
+    }
+}
